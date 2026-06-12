04d32c57e5aa590be77b198bf36afcc1bbde1605
Gabriel Alves Ribeiro <ga572017@gmail.com>
2026-06-12 02:53:43 -0300

    feat: implement mobile menu toggle functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
+// Selecionar botões de abrir e fechar o carrinho
 const cartBtn = document.getElementById('cart-btn')
 const cartSidebar = document.getElementById('cart-sidebar')
 const closeCart = document.getElementById('close-cart')
 
+// Selecionar nav e botão do menu hamburguer
+const menuBtn = document.getElementById('menu-btn')
+const nav = document.getElementById('nav')
+
+// Evento de click para abrir carrinho
 cartBtn.addEventListener('click', () =>{
     cartSidebar.classList.toggle('open')
 })
 
+// evento de click para fechar carrinho
 closeCart.addEventListener('click', () =>{
     cartSidebar.classList.remove('open')
 })
 
+// evento para abrir menu hamburguer
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('open')
+})
 
 
