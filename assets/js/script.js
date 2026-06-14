24e42eda10899ced0b2e87c97b0207d07f9380a0
Gabriel Alves Ribeiro <ga572017@gmail.com>
2026-06-13 22:56:24 -0300

    feat(cart): implement dynamic cart item creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ const closeCart = document.getElementById('close-cart')
 const menuBtn = document.getElementById('menu-btn')
 const nav = document.getElementById('nav')
 
+
+
 // Evento de click para abrir carrinho
 cartBtn.addEventListener('click', () =>{
     cartSidebar.classList.toggle('open')
@@ -22,4 +24,70 @@ menuBtn.addEventListener('click', () => {
     nav.classList.toggle('open')
 })
 
+// Selecionar botão de adicionar ao carrinho dos produtos
+const addButton = document.querySelectorAll('.add-btn')
 
+addButton.forEach((botao)=>{
+    botao.addEventListener("click", ()=>{
+
+        console.log(botao.parentElement);
+        // Configurando o elemento pai
+        const li = document.createElement('li')
+        li.classList.add('cart-item')
+        li.setAttribute('data-id', '1')
+
+        // Configurando os spans de preço e nome
+        const spanNome = document.createElement('span')
+        spanNome.classList.add('cart-item-name')
+        spanNome.innerHTML = 'Fone Bluetooth'
+
+
+
+        // Configurando os botões de controle do carrinho, incremento / decremento
+        const cartControls = document.createElement('div')
+        cartControls.classList.add('cart-item-controls')
+
+        // botão de decremento
+        const button_Decrease = document.createElement('button')
+        button_Decrease.classList.add('qty-btn')
+        button_Decrease.setAttribute('data-action', 'decrease')
+        button_Decrease.setAttribute('data-id', '1')
+        button_Decrease.innerText = '-'
+        cartControls.appendChild(button_Decrease);
+
+        // Span que mostra quantidade de itens
+        const spanQty = document.createElement('span')
+        spanQty.classList.add('cart-item-qty')
+        spanQty.innerText = '1'
+        cartControls.appendChild(spanQty);
+        
+        // botão de incremento
+        const button_Increase = document.createElement('button')
+        button_Increase.classList.add('qty-btn')
+        button_Increase.setAttribute('data-action', 'increase')
+        button_Increase.setAttribute('data-id', '1')
+        button_Increase.innerText = '+'
+        cartControls.appendChild(button_Increase);
+
+        // Indicador de preço
+        const spanPrice = document.createElement('span')
+        spanPrice.classList.add('cart-item-price')
+        spanPrice.innerText = 'R$ 299,90'
+
+        // Botão de remover item do carrinho 
+        const removeButton = document.createElement('button')
+        removeButton.classList.add('remove-btn')
+        removeButton.innerText = '🗑'
+        removeButton.setAttribute('data-id', '1')
+
+        // Elemento pai do carrinho
+        const cartList = document.getElementById('cart-list')
+
+        cartList.appendChild(li);
+        li.appendChild(spanNome);
+        li.appendChild(cartControls);
+        li.appendChild(spanPrice);
+        li.appendChild(removeButton);
+        
+    })
+})
